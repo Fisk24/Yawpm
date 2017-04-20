9d863264568c2b7b637b91a83bca34a29384115d
Fisk24 <mcfisk@ymail.com>
2017-04-19 21:32:41 -0500

    Create update-install-ubuntu.sh

---
+++ update-install-ubuntu.sh
@@ -0,0 +1,35 @@
+
+#! /bin/bash
+
+export INSTALL="/opt/Yawpm"
+
+echo "Yawpm will now attempt to update itself"
+echo
+echo "Would you like to install dependencies? (y/n): "
+# Optionaly update dependencies
+read opt
+if [[ $opt == "y" ]]; then
+	echo "Updating dependicies..."
+	sudo apt-get install python3-pyqt4 winetricks
+elif [[ $opt == "n" ]]; then
+	echo "Skipping dependency update..."
+else
+	echo "Unknown answer \"$opt\": Update Cancled!"
+	exit
+fi
+
+# Remove existing install inorder to makeway for the most upto date version
+rm -R $INSTALL
+
+# Create installation directory; CD into it; Finally, git clone into the install directory
+mkdir $INSTALL
+cd "/opt"
+
+# Download yawpm
+git clone https://github.com/Fisk24/Yawpm.git
+
+# create launcher
+cp Yawpm/yawpm.desktop /usr/share/applications/yawpm.desktop
+
+echo "Finished!"
+echo "I recommend installing mscorefonts: (Arch) yaourt ttf-ms-fonts"
